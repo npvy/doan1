cd4534c6b961d8466294e1430ce205052cdbfdf0
npvy <38617181+npvy@users.noreply.github.com>
2018-04-24 17:04:45 +0700

    Update 1712929.cpp

--- 1712929.cpp
+++ 1712929.cpp
@@ -1,4 +1,4 @@
-﻿#include <fcntl.h> //_O_U16TEXT
+#include <fcntl.h> //_O_U16TEXT
 #include <io.h>    //_setmode()
 #include <stdio.h>
 #include <iostream>
@@ -28,6 +28,96 @@ int email = 31;
 int image = 100;
 int mota = 1001;
 int sothich = 1001;
+
+void readfile(FILE *f)
+{
+	_setmode(_fileno(f), _O_U8TEXT);
+	int dem = 0;
+	wchar_t** p = (wchar_t**)malloc(20 * sizeof(wchar_t*));
+	if (f != NULL)
+	{
+		while (!feof(f))
+		{
+			p[dem] = (wchar_t*)malloc(1000 * sizeof(wchar_t));
+			fgetws(*(p + dem), 1000, f);
+			p[dem][wcslen(p[dem]) - 1] = L'\0';
+			dem++;
+			XuLiChuoi(p[dem]);
+		}
+		dem--;
+		for (int i = 0; i < dem; i++)
+		{
+			sv A;
+			A.demsothich = 0;
+			wchar_t* flag[10];
+			wchar_t* tmp;
+			// MSSV
+			if (i == 0)
+			{
+				tmp = wcstok(p[i], L",");
+				A.mssv = wcstok(NULL, L",");
+				xoakep(A.mssv);
+			}
+			else
+			{
+				A.mssv = wcstok(p[i], L",");
+				xoakep(A.mssv);
+			}
+			// Ho va ten
+			tmp = wcstok(NULL, L",");
+			A.hoten = wcstok(NULL, L",");
+			xoakep(A.hoten);
+			// Khoa
+			tmp = wcstok(NULL, L",");
+			A.khoa = wcstok(NULL, L",");
+			xoakep(A.khoa);
+			// Nien khoa
+			tmp = wcstok(NULL, L",");
+			A.nienkhoa = wcstok(NULL, L",");
+			xoakep(A.nienkhoa);
+			// Ngay sinh
+			tmp = wcstok(NULL, L",");
+			A.ngaysinh = wcstok(NULL, L",");
+			xoakep(A.ngaysinh);
+			// Hinh anh
+			tmp = wcstok(NULL, L",");
+			A.image = wcstok(NULL, L",");
+			xoakep(A.image);
+			// Email
+			tmp = wcstok(NULL, L",");
+			A.email = wcstok(NULL, L",");
+			xoakep(A.email);
+			// Mo ta ban than
+			tmp = wcstok(NULL, L",");
+			A.mota = wcstok(NULL, L",");
+			TraLaiKiTu(A.mota);
+			xoakep(A.mota);
+			// So thich
+			tmp = wcstok(NULL, L",");
+			flag[A.demsothich] = wcstok(NULL, L",");
+			while (flag[A.demsothich] != NULL)
+			{
+				A.demsothich++;
+				tmp = wcstok(NULL, L",");
+				flag[A.demsothich] = wcstok(NULL, L",");
+				xoakep(flag[A.demsothich]);
+				TraLaiKiTu(flag[A.demsothich]);
+			}
+			A.sothich = (wchar_t**)malloc(A.demsothich * sizeof(wchar_t*));
+			for (int j = 0; j < A.demsothich; j++)
+			{
+				*(A.sothich + j) = flag[j];
+			}
+			free(A.sothich);
+		}
+		fclose(f);
+	}
+	for (int i = 0; i < dem; i++)
+	{
+		free(*(p + i));
+	}
+	free(p);
+}
 void xuatdssv(sv *SV, int sosv)
 {
 	for (int i = 0; i < sosv; i++)
@@ -40,7 +130,7 @@ void xuatdssv(sv *SV, int sosv)
 		wprintf(L"\n-Ngày sinh: %ls  ", SV[i].ngaysinh);
 		wprintf(L"\n-Hình ảnh: %ls  ", SV[i].image);
 		wprintf(L"\n-Mô tả bản thân:  %ls ", SV[i].mota);
-		wprintf(L"\n");
+		wprintf(L"\n-Sở thích: ", SV[i].sothich);
 	}
 }
 void wmain()
@@ -48,27 +138,7 @@ void wmain()
 	//Doc unicode
 	_setmode(_fileno(stdout), _O_U8TEXT);
 	_setmode(_fileno(stdin), _O_U8TEXT);
-	bool kt = false;
-	FILE *doc=NULL;
-	wchar_t *lines=NULL;
-	unsigned int sosv = 0;
-	sv *SV;
-	wchar_t *fomart = L"mssv,hoten,khoa,nienkhoa,ngaysinh,image,mota,sothich";
-	sosv = demsosv(doc, kt);
-	if (kt)
-	{
-		wprintf(L"File could not be opened.\n");
-	}
-	else if (sosv == 0)
-	{
-		wprintf(L"empty\n");
-	}
-	else
-	{
-		
-		khoitao(lines, sosv);
-		xuatdssv(SV, sosv);
-		delete[]SV;
-	}
+	
 	system("pause");
 }
+
